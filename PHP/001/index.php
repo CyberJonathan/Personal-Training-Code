417d1938c68bff34f33901ef78a4e7405b0d5eef
Jonathan Santana <166652495+CyberJonathan@users.noreply.github.com>
2025-05-20 13:00:36 -0300

    20/05/2025

---
+++ PHP/001/index.php
@@ -0,0 +1,12 @@
+<?php
+    echo"Eu gosto de pizza";
+    echo"Eu realmente gosto de pizza";
+
+    $name = "Mochileiro";
+    $food = "Coxinha de Frango";
+
+        echo"OPa {$name}";
+        echo"TEm certeza que você quer a? {$food}";
+
+        
+?>
