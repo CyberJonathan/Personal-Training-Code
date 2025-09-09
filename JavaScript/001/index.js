f7bb64372ca187ac221205e2cad520e3734af991
Jonathan Santana <166652495+CyberJonathan@users.noreply.github.com>
2025-09-09 02:02:14 -0300

    09092025

--- JavaScript/001/index.js
+++ JavaScript/001/index.js
@@ -2,3 +2,16 @@ console.log(`I like pizza`);
 console.log(`but, the pizza have cheese!`);
 
 window.alert(`This is a Alert!`);
+window.alert(`So, the google console is working`);
+
+// 
+/*
+*/
+
+document.getElementById("myFirstH").textContent = `My Website`;
+document.getElementById("myFP").textContent = `Topics`;
+
+let x = 12;
+let age;
+
+age = 32;
