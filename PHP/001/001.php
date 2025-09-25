5611b2e5cfe4e40179bb0927d62ca9fe388fa86d
Jonathan Santana <166652495+CyberJonathan@users.noreply.github.com>
2025-09-24 23:53:28 -0300

    25/09/2025

---
+++ PHP/001/001.php
@@ -0,0 +1,31 @@
+<?php
+
+    $age = 30;
+
+    echo "what are your age?, is {$age} <br>";
+
+    $name = "Jhay";
+
+    echo "So your name is! {$name}<br>";
+
+    echo "My first variables!, {$age} and {$name} <br>";
+
+    $x = 5; $y = 15;
+
+    $v = $x * $y;
+
+        $p = 145; $t = 34;
+
+        $r = $p / $t;
+
+            $k = 8;  $l = 2;
+
+                $j = $k ** $l;
+
+    echo "The result 1! {$v}<br>";
+        echo "The result 2! {$r}<br>";
+            echo "The result 3! {$j}<br>";
+
+    
+
+?>
